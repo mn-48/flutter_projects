206f7d4c06b3b6944ed206175e476dba041d3427
mn-48 <nazmul.cse48@gmail.com>
2026-01-05 21:57:37 +0600

    OK

---
+++ first_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: Center(child: Text("Hello, World!")),
+      ),
+    ),
+  );
+}
